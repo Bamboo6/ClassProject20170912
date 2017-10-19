c0a77544c64f6eb129619eecb6d0f25e280abf01
Bamboo6 <1937074453@qq.com>
2017-10-19 08:34:41 +0800

    创建20171016所需文件

---
+++ app/src/main/java/cn/edu/gdmec/android/classproject20170912/App.java
@@ -0,0 +1,10 @@
+package cn.edu.gdmec.android.classproject20170912;
+
+import android.app.Application;
+
+/**
+ * Created by lt on 2017/10/19.
+ */
+
+public class App extends Application {
+}
---
+++ app/src/main/java/cn/edu/gdmec/android/classproject20170912/m2theftguard/ContactSelectActivity.java
@@ -0,0 +1,15 @@
+package cn.edu.gdmec.android.classproject20170912.m2theftguard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.edu.gdmec.android.classproject20170912.R;
+
+public class ContactSelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_select);
+    }
+}
